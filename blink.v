285998d7d469c78942ce0305945b621619c694bf
Vinícius César Bertolo Calligaris <vinicius.cesar7@hotmail.com>
2025-06-03 12:20:43 -0300

    Teste 5

--- blink.v
+++ blink.v
@@ -6,24 +6,29 @@ module Blink #(
     output reg [7:0] leds
 );
 
-localparam ONE_SECOND  = CLK_FREQ;
-localparam HALF_SECOND = CLK_FREQ / 2;
+localparam ONE_SECOND  = CLK_FREQ 9 / 10;
+localparam HALF_SECOND = CLK_FREQ 2 / 10;
 
 reg [31:0] counter;
+reg state_on; 
 
-always @(posedge clk ) begin
+always @(posedge clk) begin
     if (!rst_n) begin
-        counter <= 32'h0;
-        leds     <= 8'b00000001;
+        counter <= 0;
+        leds <= 8'b00000000;
+        state_on <= 1'b1; 
     end else begin
-        if (counter >= HALF_SECOND - 1) begin
-            counter <= 32'h0;
-            leds <= leds ^ 8'b01010101;
-        end else begin
-            counter <= counter + 1;
+        counter <= counter + 1;
+
+        if (state_on && counter >= ONE_SECOND) begin
+            counter <= 0;
+            state_on <= 0;
+            leds[0] <= 0;  
+        end else if (!state_on && counter >= HALF_SECOND) begin
+            counter <= 0;
+            state_on <= 1;
+            leds[0] <= 1; 
         end
     end
-end
-    
-    
+end  
 endmodule
